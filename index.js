b14c8e16c50a665b8fd6d1c43a6c2d7134da7a74
zhongshan <imzhongshan@126.com>
2018-05-06 23:47:05 +0800

    first

---
+++ index.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/Counter'
+import counter from './reducers'
+
+const store = createStore(counter)
+/*const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  rootEl
+)
+
+render()
+store.subscribe(render)*/
+
+export default class DoubleRedux extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            change: false
+        }
+    }
+
+    rerender() {
+        this.setState({
+            change: !this.state.change
+        })
+    }
+
+    componentWillMount() {
+        store.subscribe(this.rerender)
+    }
+
+    render() {
+        return <Counter value={store.getState()} onIncrement={() => store.dispatch({ type: 'INCREMENT' })} onDecrement={() => store.dispatch({ type: 'DECREMENT' })} />
+    }
+}
